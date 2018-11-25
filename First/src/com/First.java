8a3f26bbf8ebe139f9cdb97526bcf2a733fe31cf
raghavendra-chervirala <raghavendrasourav@gmail.com>
2018-11-25 18:22:12 +0530

    FirstCommit

---
+++ First/src/com/First.java
@@ -0,0 +1,15 @@
+package com;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          System.out.println("Hellow");
+          
+          for(int i=1;i<=10;i++)
+          {
+        	  System.out.println(i);
+          }
+	}
+
+}
